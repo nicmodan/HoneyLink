a6779078fc44a8b4711b1647274b3ab471832c3c
De-bby4 <chiamakadeborah019@gmail.com>
2026-03-12 12:43:44 +0100

    correction index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,41 +1,20 @@
 import { Image } from 'expo-image';
 import { Platform, StyleSheet, StatusBar } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-// import { Text } from '@react-navigation/elements';
 import { View, Text, TextInput, Pressable  } from 'react-native';
 
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <View style={styles.innerContainer}>
-
-        <View style={styles.centerTextContainer}>
-          <Text style={styles.displayText}>
-            Find Your Perfect March
-          </Text>
-          <Pressable style={{marginBottom: 10, backgroundColor: "pink", borderRadius: 10}}>
-            <Text style={{color: '#000000', fontSize: 18, fontWeight: 'bold'}}>
-              Get Started
-            </Text>
-          </Pressable>
-          <Pressable style={{marginBottom: 10, backgroundColor: "gray", borderRadius: 10}}>
-            <Text style={{color: '#000000', fontSize: 18, fontWeight: 'bold'}}>
-              Get Started
-            </Text>
-          </Pressable>
-
-        </View>
-        
-       
-
-      </View>
-      
+    <Image
+      source= {require('../../assets/images/sample.jpg' )}
+      // style={styles?}
+    />   
+    <view 
+      // style={styles.subContainer}
+    >
+    
+    </view>
     </View>
   );
 }
@@ -49,9 +28,7 @@ const styles = StyleSheet.create({
     position: 'relative',
     width: '100%',
     height: '100%',
-    backgroundImage: 'url(../assets/images/frontimage.png)',
-    // backgroundSize: 'cover',
-    // backgroundAttachment: 'fixed',
+    backgroundImage: 'red',
     ...Platform.select({
       ios:{
         paddingTop: 25
@@ -61,16 +38,12 @@ const styles = StyleSheet.create({
       }
     })
   },
-  innerContainer: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    position: 'absolute',
-    width: '100%',
-    height: '100vh',
-    backgroundImage: 'url(../assets/images/frontimage.png)',
-    // backgroundColor: 'rgba(197, 43, 43, 0.17)', // Adjust the opacity as needed
-  },
+backgroungImageStyle: {
+  width: '100%',
+  height: '100%',
+  resizeMode: 'cover'
+
+},
   cneterTextContiner: {
     flex: 1,
     alignItems: 'center',
