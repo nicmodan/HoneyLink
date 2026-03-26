54b6b65e1e9027630c8d6809ebeef48554f22887
Divine Favour <divinefavour5793@gmail.com>
2026-03-26 13:56:56 +0100

    login Userinfro

--- scripts/apollo.js
+++ scripts/apollo.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "http://10.49.244.215:4000/graphql",
 });
 
 const client = new ApolloClient({
